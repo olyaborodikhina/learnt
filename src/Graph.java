22c0b4395da48ca4021d9f8880dffc638a884a92
olyaborodikhina <020189kirill>
2017-01-13 15:30:17 +0600

    !

--- src/Graph.java
+++ src/Graph.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
-
-import static java.text.ChoiceFormat.nextDouble;
 
 /**
  * Created by hp on 10.12.2016.
@@ -11,7 +10,7 @@ import static java.text.ChoiceFormat.nextDouble;
 public class Graph extends JFrame {
     final static int COUNT_POINTS = 30;
     final static int LENGTH_X = 1500;
-    final static int DEGREE = 4;
+    final static int DEGREE = 9;
 
     static ArrayList<MyPoints> points = new ArrayList<>();
     static ArrayList<MyPoints> pointsForGraph = new ArrayList<>();
@@ -24,18 +23,17 @@ public class Graph extends JFrame {
         graph.setBackground(Color.WHITE);
 
         for(double i = -Math.PI; i < 2 * Math.PI; i += 0.002){
-            double xx = 200 + i * 200; //для func1, func 3
-            double yy = 300 - graph.func1(i) * 200; //для func1, func 3
+            //double xx = 200 + i * 200; //для func1, func 3
+           // double yy = 300 - graph.func1(i) * 200; //для func1, func 3
+
+            double xx = 500 + i * 200 * 0.03; // для func 2
+            double yy = 300 - graph.func2(i) * 200 * 0.03; // для func 2
             pointsForGraph.add(new MyPoints(xx, yy));
-
-
-        //int xx = 500 + i * 200 * 0.03; // для func 2
-            // int yy = 300 - func2(i) * 200 * 0.03; // для func 2
         }
 
         for (int i = 0; i < COUNT_POINTS; i++) {
-            MyPoints point = graph.generateNormalSample();
-            //MyPoints point = graph.generateSamle();
+            //MyPoints point = graph.generateNormalSample();
+            MyPoints point = graph.generateSamle();
             points.add(point);
         }
 
@@ -45,12 +43,11 @@ public class Graph extends JFrame {
             System.out.println("w" + i + "= " + w[i]);
 
      for(double i = -Math.PI; i < 2 * Math.PI; i += 0.002){
-         double xx = 200 + i * 200; //для func1, func 3
-         double yy = 300 - graph.funcW(i,w,DEGREE) * 200; //для func1, func 3
-
+        // double xx = 200 + i * 200; //для func1, func 3
+         //double yy = 300 - graph.funcW(i,w,DEGREE) * 200; //для func1, func 3
+         double xx = 500 + (int)(i * 200 * 0.03); // для func 2
+         double yy = 300 - (int)graph.funcW(i,w,DEGREE) * 200 * 0.03; // для func 2
          pointsForGraph2.add(new MyPoints(xx,yy));
-         //int xx = 500 + (int)(i * 200 * 0.03); // для func 2
-         // int yy = 300 - (int)(func2(i) * 200 * 0.03); // для func 2
      }
 
         System.out.println("crossvalidate = " + graph.crossValidate(pointsGoodFromSample));
@@ -78,16 +75,15 @@ public class Graph extends JFrame {
      for(int i = 0; i < pointsForGraph2.size(); i++){
          graphics.drawOval((int)(pointsForGraph2.get(i).x - 1/2),(int) (pointsForGraph2.get(i).y - 1/2), 2,2);
      }
-
        //выводим кроссвалидацию
-   }
+ }
 
    public MyPoints graphics(double x, double t){
        MyPoints point = new MyPoints();
-       double xx = 200 + x *200;
-       double yy = 300 - t*200;
-       //int xx = 500 + (int)(i * 200 * 0.03); // для func 2
-       // int yy = 300 - (int)(func2(i) * 200 * 0.03); // для func 2
+       //double xx = 200 + x *200;
+       //double yy = 300 - t*200;
+       double xx = 500 + (int)(x * 200 * 0.03); // для func 2
+       double yy = 300 - (int)(t * 200 * 0.03); // для func 2
        point.x = xx;
        point.y = yy;
        return point;
@@ -99,7 +95,7 @@ public class Graph extends JFrame {
    }
 
    public double func2(double x){
-       return 5*Math.pow(x,3) + Math.pow(x,2) + 5;
+       return 5*Math.pow(x,6) + Math.pow(x,2) + 5;
    }
 
    public double func3(double x){
@@ -111,7 +107,6 @@ public class Graph extends JFrame {
       double t;
        double e = 0.0;
        double s = 0.0;
-       Point point = new Point();
        Random random = new Random();
 
        int rand = random.nextInt(getWidth());
@@ -122,7 +117,7 @@ public class Graph extends JFrame {
            s = 1.;
        }else
            s = -1;
-       t = func1(x) + s*e ;
+       t = func2(x) + s*e ;
        pointsGoodFromSample.add(new MyPoints(x,t));
        return graphics(x,t);
 
@@ -134,19 +129,19 @@ public class Graph extends JFrame {
        double t;
        int rd = random.nextInt(LENGTH_X);
        x = (2 * Math.PI *rd) / LENGTH_X;
-       t = func1(x) + random.nextGaussian()*0.2;
+       t = func2(x) + random.nextGaussian()*0.2;
        pointsGoodFromSample.add(new MyPoints(x,t));
        return graphics(x,t);
    }
 
 
-    public double[] builtAndDecisionPolinom(ArrayList<MyPoints> points, int polinomGegree,int count){ //составляем матрицы A,B
-        double [][] A = new double[polinomGegree+1][polinomGegree+1];
-        double [] B = new double[polinomGegree+1];
+    public double[] builtAndDecisionPolinom(ArrayList<MyPoints> points, int polinomDegree,int count){ //составляем матрицы A,B
+        double [][] A = new double[polinomDegree+1][polinomDegree+1];
+        double [] B = new double[polinomDegree+1];
         double [] w;
 
-        for(int i = 0; i < polinomGegree + 1; i++){
-            for(int j = 0; j < polinomGegree + 1; j++) {
+        for(int i = 0; i < polinomDegree + 1; i++){
+            for(int j = 0; j < polinomDegree + 1; j++) {
                 A[i][j] = getSumX(points, i+j);
             }
             for(int k = 0; k < count; k++)
@@ -154,11 +149,11 @@ public class Graph extends JFrame {
         }
        //решаем методом гаусса
 
-        w = decisionPolinomByGauss(A,B,polinomGegree);
+        w = decisionPolinomByGauss(A,B,polinomDegree);
         return w;
     }
 
-    private double getSumX(ArrayList<MyPoints> point, int power){
+    private double getSumX(ArrayList<MyPoints> point, int power){ //sum x^power
         double result = 0;
         for(int i = 0; i < point.size(); i++){
             result+= Math.pow(point.get(i).getX(), power);
@@ -166,7 +161,7 @@ public class Graph extends JFrame {
         return result;
     }
 
-    public double[] decisionPolinomByGauss(double [][]A, double []B, int polinomDegree){
+    public double[] decisionPolinomByGauss(double [][]A, double []B, int polinomDegree){ //
         double[]w = new double[polinomDegree+1];
         //решение полинома
         double d = 0, s = 0;
@@ -197,7 +192,8 @@ public class Graph extends JFrame {
         double error = 0;
         double [] t2 = new double[COUNT_POINTS - 1];
         double [] x2 = new double[COUNT_POINTS - 1];
-
+        double curError = 0;
+        ArrayList<Double> errors = new ArrayList<>();
 
         double [] x = new double[COUNT_POINTS];
         double [] t = new double[COUNT_POINTS];
@@ -206,37 +202,56 @@ public class Graph extends JFrame {
             t[i] = points.get(i).y;
         }
 
-        for(int curPoint = 0; curPoint < COUNT_POINTS; curPoint++){
+        for(int degree = 1; degree < DEGREE; degree++) {
+            error = 0;
+            for (int curPoint = 0; curPoint < COUNT_POINTS; curPoint++) {
 
-            for(int i = 0; i < curPoint; i++){
-                x2[i] = x[i];
-                t2[i] = t[i];
+                for (int i = 0; i < curPoint; i++) {
+                    x2[i] = x[i];
+                    t2[i] = t[i];
 
+                }
+                for (int i = curPoint + 1; i < COUNT_POINTS; i++) {
+                    x2[i - 1] = x[i];
+                    t2[i - 1] = t[i];
+                }
+
+                //Для нахождения w используются все точки, кроме одной!
+                ArrayList<MyPoints> pointses = new ArrayList<>();
+                for (int i = 0; i < t2.length; i++) {
+                    pointses.add(new MyPoints(x2[i], t2[i]));
+                }
+
+                double[] w = builtAndDecisionPolinom(pointses, degree, COUNT_POINTS - 1);
+                double y = funcW(x[curPoint], w, degree);
+                curError = Math.abs(t[curPoint] - y);
+                error += curError;
             }
-            for(int i = curPoint+1; i < COUNT_POINTS; i++){
-                x2[i-1] = x[i];
-                t2[i-1] = t[i];
-            }
+            errors.add(error);
 
-            //Для нахождения w используются все точки, кроме одной!
-            ArrayList<MyPoints> pointses = new ArrayList<>();
-            for(int i = 0; i < t2.length; i++){
-                pointses.add(new MyPoints(x2[i],t2[i]));
-            }
-
-            double [] w = builtAndDecisionPolinom(pointses, DEGREE-1,COUNT_POINTS-1);
-            double y = funcW(x[curPoint], w,DEGREE-1);
-            error += Math.abs(t[curPoint] - y);
         }
-        return error /= COUNT_POINTS;
+
+        int index = 0;
+        double minError = Double.MAX_VALUE;
+        for(int i = 0; i < errors.size(); i++){
+            double min = errors.get(i);
+            if(min < minError) {
+                minError = min;
+                index = i + 1;
+            }
+        }
+
+        return  index;
+
+
+      //  return error /= COUNT_POINTS;
     }
 
-    public double funcW(double x, double [] w, int polinomGedree){
+    public double funcW(double x, double [] w, int polinomDegree){ //считаем значение полинома w при известных x w
         double res = 0;
-        for(int i = 0; i < polinomGedree+1; i++){
+        for(int i = 0; i < polinomDegree+1; i++){
             res+= w[i] * Math.pow(x, i);
         }
         return res;
-
     }
 }
